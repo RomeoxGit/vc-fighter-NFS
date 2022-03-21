c605b2d983d5f833d6b89453544537218ce1e84d
RomeoxGit <98481727+RomeoxGit@users.noreply.github.com>
2022-03-21 23:10:51 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -10,7 +10,7 @@ from .. import vcbot, SUDO_USERS, HNDLR, hl, START_VID
    # video=START_VID,
   # await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
 
-START_MSG = "**Hello [{}](tg://user?id={}) !** \n\n __ • I'm ArrayCore An Advance And Simple Group Voice Call Bot__ \n\n **Click Below Buttons for More Info**",         
+START_MSG = "**𝗛𝖾𝗅𝗅𝗈 [{}](tg://user?id={}) !** \n\n __ • 𝖨'𝗆 𝖠𝗇 𝖠𝖽𝗏𝖺𝗇𝖼𝖾 𝖡𝗈𝗍 𝖳𝗈 𝖥𝗎𝖼𝗄 𝖳𝗁𝖾 𝘃𝗰 𝗙𝗂𝗀𝗁𝗍𝖾𝗋𝗌\𝗇 𝗗𝖾𝗌𝗂𝗀𝗇𝖾𝖽 𝗕𝗒 𝐌𝐫. 𝗥𝗈𝗆𝖾𝗈 <\🇮🇳**",         
 Hn = "/"
 @vcbot.on_message(filters.private & filters.incoming & filters.command(['start'], prefixes=Hn))
 async def _start(_, ok: Message):
@@ -19,11 +19,11 @@ async def _start(_, ok: Message):
             reply_markup=InlineKeyboardMarkup(
                 [[
                     InlineKeyboardButton(
-                        "• Channel •", url="https://t.me/ArrayCore"),
+                        "𝗨𝗉𝖽𝖺𝗍𝖾𝗌", url="https://t.me/NFSxBOTZ"),
                     InlineKeyboardButton(
-                        "• Support •", url="https://t.me/DNHxHELL")
+                        "𝗦𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/NFSxSUPPORT")
                 ], [
                     InlineKeyboardButton(
-                        "• Repo •", url="https://github.com/desinobita/TgVcUsers")
+                        "𝗥𝖾𝗉𝗈", url="https://t.me/Its_romeoo")
                 ]]
             ))
